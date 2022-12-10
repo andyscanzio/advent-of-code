7112692ae51c62e69a8442d5bf5f4571cef53f66
Andrea Scanzio <andyscanzio@gmail.com>
2022-12-10 21:23:58 +0100

    Rebase

--- 2022/src/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def load_input(day: str) -> str:
-    __location__ = os.path.realpath(os.path.join(os.getcwd()))
-    path = os.path.join(os.getcwd(), f"2022/inputs/input_{day}.txt")
-    with open(path, "r") as f:
-        return f.read()
