4394f113c9f4476bdd3897d71cfbbaa188c666fe
Andrea Scanzio <andyscanzio@gmail.com>
2022-12-11 01:07:23 +0100

    Use pathlib

--- utils.py
+++ utils.py
@@ -1,8 +1,7 @@
-import os
+from pathlib import Path
 
 
 def load_input(day: str) -> str:
-    __location__ = os.path.realpath(os.path.join(os.getcwd()))
-    path = os.path.join(os.getcwd(), f"inputs/input_{day}.txt")
+    path = Path.cwd() / Path(f"inputs/input_{day}.txt")
     with open(path, "r") as f:
         return f.read()
